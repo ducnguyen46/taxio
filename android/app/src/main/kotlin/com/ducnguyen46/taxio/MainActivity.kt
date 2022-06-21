3c8223598045bd0e252ad8f17e8dbd34a26e90a7
nnduc2 <nguyenngocduccl@gmail.com>
2022-06-21 17:29:52 +0700

    initial

---
+++ android/app/src/main/kotlin/com/ducnguyen46/taxio/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ducnguyen46.taxio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
